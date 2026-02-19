80ad6a3adfcec74fc0251f6674b44100c80e5c25
UmerSR <umerraja2003@gmail.com>
2026-02-19 09:50:08 +0500

    refactor: remove redundant files

--- Development/Sprint-3/code/chartspec_donor/frontend/components/workspace/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as TableProfilingTab } from './TableProfilingTab';
-export { default as ColumnProfilingTab } from './ColumnProfilingTab';
-export { default as ChartBuilderTab } from './ChartBuilderTab';
